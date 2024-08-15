662d23342e5f6ed1aeba376931d04461efb71390
dipaloke <dipalokebiswas96@gmail.com>
2024-08-16 04:02:34 +0600

    feat (schema for collection, Item, Comment, Like): Created schema models for Collections with custom fields, Items, Like and Comments.

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,4 @@
-// import { Pool, neonConfig } from "@neondatabase/serverless";
-// import { PrismaNeon } from "@prisma/adapter-neon";
-
-// // import dotenv from "dotenv";
-// import ws from "ws";
 import { PrismaClient } from "@prisma/client";
-
-// dotenv.config();
-
-// neonConfig.webSocketConstructor = ws;
-
-// const connectionString = process.env.DATABASE_URL as string;
-
-//  if (!connectionString) {
-//   console.error("cant get DATABASE_URL")
-//    throw new Error("DATABASE_URL IS MISSING");
-// }
-
-// const pool = new Pool({ connectionString });
-// const adapter = new PrismaNeon(pool);
-// export const db = new PrismaClient({ adapter });
 
 declare global {
   var prisma: PrismaClient | undefined;
